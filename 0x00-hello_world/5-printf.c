2aa2599ef6f88238f6a43373fab12b1d45eef55d
Ajide Tomisin <ajidetomisin86@gmail.com>
2022-06-30 19:37:12 -0700

    learing c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,8 @@
-#!/bin/bash
+#include <stdio.h>                                                                                                                      
+/*main.c*/
+int main(void)                                                                                                                          
+{                                                                                                                                       
+
+	        printf("with proper grammar, but the outcome is a piece of art,");
+		return (0); 
+}
