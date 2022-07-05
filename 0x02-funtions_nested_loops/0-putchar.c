d50e1b3404e2245d619ec02c07efa0af72721d4f
Ajide Tomisin <ajidetomisin86@gmail.com>
2022-07-05 14:32:09 -0700

    coding

---
+++ 0x02-funtions_nested_loops/0-putchar.c
@@ -0,0 +1,9 @@
+/**
+ * main - this a the main function
+ * description - the starting point of the program
+ */
+int main(void)
+{
+	putchar ("\_putchar");
+	return (0);
+}
---
+++ 0x02-funtions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int main(void)
+{
+	char ch = 'a';
+	while(ch <= 'z')
+	{
+		putchar("%c", ch);
+		ch++;
+	}
+
+	returnn (0);
