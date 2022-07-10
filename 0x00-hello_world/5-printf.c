b647660a49eff4d21208a5bcc8a9345eaa0877f0
Ajide Tomisin <ajidetomisin86@gmail.com>
2022-07-10 08:02:29 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>                                                                                                                      
-/*main.c*/
+/**
+ * main - entry poin
+ *
+ * return: 0
+ */
 int main(void)                                                                                                                          
 {                                                                               
           	printf("with proper grammar, but the outcome is a piece of art,\n");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,29 @@
-#!/bin/bash
+include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int a;
+		long int l;
+		long long int d;
+		char c;
+
+		float f;
+		printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+		printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+		printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+		return (0);
+}
