95148a463a7b64ac97ad3e65472fb24c76b927dc
Ajide Tomisin <ajidetomisin86@gmail.com>
2022-07-13 06:42:59 -0700

    loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowc;
+
+	for (lowc = 'a', lowc <= 'z', lowc++)
+		putchar(lowc);
+		putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowc, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (lowc = 'a'; lowc <= 'z'; low++)
+	{
+		if (lowc != e && lowc != q)
+			putchar(lowc);
+	}
+	putchar('\')
+	return (0);
+}
+
+
