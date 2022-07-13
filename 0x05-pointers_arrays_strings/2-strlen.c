a4ccb75e66586af52a6f35e4f4306279d7b1874b
Ajide Tomisin <ajidetomisin86@gmail.com>
2022-07-13 10:10:06 -0700

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+ *_strlen - ounting strings
+ *@s: inputed string
+ *Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
